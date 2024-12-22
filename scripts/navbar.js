cc1918c7d32c1459cecacd1b115ef2da1a2edd8e
Utkarsh Bajaj <ubajaj99@gmail.com>
2024-12-22 16:40:31 +0530

    Created a navbar and added functional as a subject (respect)

---
+++ scripts/navbar.js
@@ -0,0 +1,11 @@
+document.getElementById("navbar").innerHTML = `
+  <div class="links">
+    <span><a href="index.html">home</a> /</span>
+    <span><a href="work.html">work ex</a> /</span>
+    <span><a href="projects.html">projects</a> /</span>
+    <span><a href="thoughts-and-goals.html">thoughts & goals</a> /</span>
+    <span><a href="academia.html">academia</a> /</span>
+    <span><a href="contact.html">apply</a> /</span>
+    <span><a href="../assets/Resume.pdf">resume</a> /</span>
+  </div>
+`;
